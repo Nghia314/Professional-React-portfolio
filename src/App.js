0c5c5e2db3ccfdf7d529e171c792c7c11f2c51d8
Nghia Le <davidle314@hotmail.com>
2022-05-30 02:26:33 -0700

    added file on project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
