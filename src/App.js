de63baac5d4af0f8a64601e4ba91e71fc3a211fe
Nghia Le <davidle314@hotmail.com>
2022-05-30 02:37:35 -0700

    update ffile with index.js and css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <Header></Header>
+      <Footer ></Footer>
     </div>
   );
 }
