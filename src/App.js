e24022b995bf4bff85c9ce441f5091325651ad79
Nghia Le <davidle314@hotmail.com>
2022-05-31 09:53:50 -0700

    added index page

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ function App() {
 
         <AnimatePresence exitBeforeEnter>
           <Switch location={location} key={location.pathname}>
-            <Route exact path="/" component={Main} />{" "}
-            <Route exact path="/about" component={AboutPage} />{" "}
-            <Route exact path="/blog" component={BlogPage} />{" "}
-            <Route exact path="/work" component={WorkPage} />{" "}
+            <Route exact path="/" component={Main} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/blog" component={BlogPage} />
+            <Route exact path="/work" component={WorkPage} />
             <Route exact path="/skills" component={MySkillsPage} />
-          </Switch>{" "}
+          </Switch>
         </AnimatePresence>
       </ThemeProvider>
     </>
