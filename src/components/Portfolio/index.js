647ded8e72fc73b23a21b8d9ac19806c2dca4412
Nghia Le <davidle314@hotmail.com>
2022-05-31 22:38:51 -0700

    added picture for project

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ProjectCards from ".//Project";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const portfolio =[
+    {
+        "id":1,
+        "name":"ARTKetplace"
+        "image":"/im"
+        "github":"https://github.com/Nghia314/artketplace"
+        "deploy":"https://artketplace.herokuapp.com"
+    }
+    {
+        "id":2,
+        "name":"The Travel Center"
+        "image":
+        "github":"https://github.com/Nghia314/the-travel-center"
+        "deploy":"https://nghia314.github.io/the-travel-center/"
+    }
+    {
+        "id":3,
+        "name":"Note Taker"
+        "image":
+        "github":"https://github.com/Nghia314/Note-Taker"
+        "deploy":"https://note-taker-aop.herokuapp.com/"
+    }
+    {
+        "id":4,
+        "name":"E-Commerce-Backend"
+        "image":
+        "github":"https://github.com/Nghia314/E-commerce-backend"
+        "video":"https://www.youtube.com/watch?v=ts8AyJVCEOQ"
+    }
+]
