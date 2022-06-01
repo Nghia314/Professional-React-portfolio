e35fe8bd35dd69317c134bc2d9f9c33b5ab0bede
Nghia Le <davidle314@hotmail.com>
2022-05-31 23:09:58 -0700

    added resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Nghia Le</h2>
+          <ul>
+            <li>
+              Manager Construction , full-stack web developer
+              
+            </li>
+          </ul>
+
+          <p class="mt-5">
+            <a href="https://www.linkedin.com/in/nghia-le-4a2b9b232/">
+              <img
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="inkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href="https://docs.google.com/document/d/1i1ZPNqccVKauzvgAOQi6r9cdVlGoepyu/edit?usp=sharing"
+            class="link"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title">Proficiencies</h2>
+          <ul>
+            <li>
+            HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js,<br />
+            Node.js, Express.js, SQL, Sequelize, MongoDB, Mongoose, APIs, Templating.
+            
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Resume;
