3632c97e8da328ab4404a0debc454a6c558ab9f3
Nghia Le <davidle314@hotmail.com>
2022-05-31 19:44:14 -0700

    update the main page with about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,8 +6,13 @@ function About() {
             <h2 className="nav-title-font">Nghia Le</h2>
             <hr />
             <div>
-                <img className="mb-5" id="avatar" src={process.env.PUBLIC_URL + '/'}
+                <img className="mb-5" id="avatar" src={process.env.PUBLIC_URL + '/Assets/portfolio.webp'} alt="Nghia Le" />
+                <p>
+                    My name is Nghia Le and I have 3+ years of manager constrctions and I'm a student at Berkeley University bootcamp pursuing Full stack web development.
+                </p>
             </div>
         </section>
     )
 }
+
+export default About;
