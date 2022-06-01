e456ab35319fd57c6c1c98af634a6375112b6d71
Nghia Le <davidle314@hotmail.com>
2022-05-31 20:41:36 -0700

    update footer with github and linked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 import React from "react";
 
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="ray">
---
+++ src/components/Footer.js/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer className="footer fixed-bottom">
+            <a href="https://github.com/Nghia314"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/nghia-le-4a2b9b232/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="Github" className="icon"/></a>
+            <a href="Mailto:Davidle314@hotmail.com"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-email-notifications-justicon-flat-justicon.png" alt="Github" className="icon"/></a>
+            <a href="tel:9168907740"><img src="https://img.icons8.com/external-flat-juicy-fish/60/000000/external-phone-contact-us-flat-flat-juicy-fish-2.png" alt="Github" className="icon"/></a>
+        </footer>
+    )
+}
+export default Footer;
