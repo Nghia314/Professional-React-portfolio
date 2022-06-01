a19e7ed96f10199be565f236051e9f5e4bc99088
Nghia Le <davidle314@hotmail.com>
2022-05-31 22:48:02 -0700

    added data for portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,30 +5,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const portfolio =[
     {
         "id":1,
-        "name":"ARTKetplace"
-        "image":"/im"
-        "github":"https://github.com/Nghia314/artketplace"
+        "name":"ARTKetplace",
+        "image":"/images/artketplace.png",
+        "github":"https://github.com/Nghia314/artketplace",
         "deploy":"https://artketplace.herokuapp.com"
     }
     {
         "id":2,
-        "name":"The Travel Center"
-        "image":
-        "github":"https://github.com/Nghia314/the-travel-center"
+        "name":"The Travel Center",
+        "image":"/images/the-travel-center.png",
+        "github":"https://github.com/Nghia314/the-travel-center",
         "deploy":"https://nghia314.github.io/the-travel-center/"
     }
     {
         "id":3,
-        "name":"Note Taker"
-        "image":
-        "github":"https://github.com/Nghia314/Note-Taker"
+        "name":"Note Taker",
+        "image":"/images/note-taker.png",
+        "github":"https://github.com/Nghia314/Note-Taker",
         "deploy":"https://note-taker-aop.herokuapp.com/"
     }
     {
         "id":4,
-        "name":"E-Commerce-Backend"
-        "image":
-        "github":"https://github.com/Nghia314/E-commerce-backend"
+        "name":"E-Commerce-Backend",
+        "image":"/images/e-commerce-backend.png",
+        "github":"https://github.com/Nghia314/E-commerce-backend",
         "video":"https://www.youtube.com/watch?v=ts8AyJVCEOQ"
     }
 ]
+function Wrapper(props) {
+    return <div className="wrapper">{props.children}</div>
+}
+
+function Portfolio() {
+    return (
+        <section className="container">
+            <div className="project">
+                <h2 className="top-title">Portfolio</h2>
+                <hr />
+            </div>
+
+            <Wrapper id="card-data">
+                {portfolio.map((project) => (
+                    <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} video={project.video} />
+
+                ))}
+            </Wrapper>
+        </section>
+    )
+}
+
+export default Portfolio;
